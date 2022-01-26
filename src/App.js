672021663efdd1d6c20e125ee4eb5096de923c23
Gasper Katrasnik <gkatrasnik@gmail.com>
2022-01-26 17:51:40 +0100

    some style added

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
     setUsers(usersdata);
     setTasks(tasksdata);
     populateShowTasksList();
-    console.log(uuid());
+    // random uuid console.log(uuid());
   }, []);
 
   return (
