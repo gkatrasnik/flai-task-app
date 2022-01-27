ea8ef2b8892847e2cec4d134ef432d25be286742
Gasper Katrasnik <gkatrasnik@gmail.com>
2022-01-27 21:38:02 +0100

    conflicts fixed

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -55,10 +55,7 @@ function AddTask(props) {
           value={topic}
           onChange={(e) => setTopic(e.target.value)}
         />
-<<<<<<< HEAD
-=======
 
->>>>>>> 72e53f5fffa602fee02317965f2d7e0c59e4be47
         <label for="description">Description:</label>
         <textarea
           type="text"
